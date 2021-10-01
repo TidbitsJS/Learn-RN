5e6a4b8adc0de41c2099d826bf1eea9c96f92283
Tidbits JS <tidbitsjs@gmail.com>
2021-10-01 22:37:20 +0530

    home header component

---
+++ foodie/components/HomeHeader.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { COLORS, FONTS, icons, SIZES } from "../constants";
+
+const HomeHeader = ({ currentLocation }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: SIZES.font,
+        paddingHorizontal: SIZES.font,
+        height: "auto",
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={icons.nearby}
+          resizeMode="contain"
+          style={{
+            width: 25,
+            height: 25,
+          }}
+        />
+      </TouchableOpacity>
+
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            width: "70%",
+            paddingVertical: SIZES.base,
+            paddingHorizontal: SIZES.font,
+            backgroundColor: COLORS.lightGray3,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: SIZES.radius,
+          }}
+        >
+          <Text style={{ ...FONTS.body4 }} numberOfLines={1}>
+            {currentLocation.streetName}
+          </Text>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={icons.basket}
+          resizeMode="contain"
+          style={{
+            width: 23,
+            height: 23,
+          }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default HomeHeader;
--- foodie/screens/Home.js
+++ foodie/screens/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import HomeHeader from "../components/HomeHeader";
 import { FONTS, COLORS } from "../constants";
 import FocusedStatusBar from "../utils/FocusedStatusBar";
 
+const initialCurrentLocation = {
+  streetName: "123 Baker Street, London",
+  gps: {
+    latitude: 1.5496614931250685,
+    longitude: 110.36381866919922,
+  },
+};
+
 const Home = () => {
+  const [currentLocation, setCurrentLocation] = React.useState(
+    initialCurrentLocation
+  );
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar animated={true} backgroundColor={COLORS.lightGray} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightGray4 }}>
+      <FocusedStatusBar
+        animated={true}
+        backgroundColor={COLORS.lightGray4}
+        barStyle="dark-content"
+      />
       <View style={{ flex: 1 }}>
-        <Text style={{ ...FONTS.h1 }}>Foodie</Text>
+        <HomeHeader currentLocation={currentLocation} />
       </View>
     </SafeAreaView>
   );
