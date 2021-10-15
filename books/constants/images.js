59a899371b47826387406b65d7e0ff9d87a17706
Tidbits JS <tidbitsjs@gmail.com>
2021-10-15 18:40:40 +0530

    book data

--- books/constants/images.js
+++ books/constants/images.js
@@ -2,10 +2,36 @@ export const otherWordsForHome = require("../assets/images/other_words_for_home.
 export const theMetropolist = require("../assets/images/the_metropolist.jpg");
 export const theTinyDragon = require("../assets/images/the_tiny_dragon.jpg");
 export const underland = require("../assets/images/underland.jpg");
+export const atomicHabits = require("../assets/images/atomic_habits.jpg");
+export const cantHurtMe = require("../assets/images/cant_hurt_me.jpg");
+export const chamberOfSecrets = require("../assets/images/chamber_of_secrets.jpg");
+export const daVinciCode = require("../assets/images/da_vinci_code.jpg");
+export const everythingIs = require("../assets/images/everything_is.jpg");
+export const theHobbit = require("../assets/images/the_hobbit.jpg");
+export const faultInOurStars = require("../assets/images/fault_in_our_stars.jpg");
+export const fortyRulesOfLove = require("../assets/images/forty_rules_of_love.jpg");
+export const meluha = require("../assets/images/meluha.jpg");
+export const palaceOfIllusions = require("../assets/images/palace_of_illusions.jpg");
+export const sita = require("../assets/images/sita.jpg");
+export const subtleArt = require("../assets/images/subtle_art.jpg");
+export const wishICouldTellYou = require("../assets/images/wish_i_could_tell_you.jpg");
 
 export default {
-    otherWordsForHome,
-    theMetropolist,
-    theTinyDragon,
-    underland
-}
+  otherWordsForHome,
+  theMetropolist,
+  theTinyDragon,
+  underland,
+  atomicHabits,
+  cantHurtMe,
+  chamberOfSecrets,
+  daVinciCode,
+  theHobbit,
+  everythingIs,
+  faultInOurStars,
+  fortyRulesOfLove,
+  meluha,
+  palaceOfIllusions,
+  sita,
+  subtleArt,
+  wishICouldTellYou,
+};
---
+++ books/data/books.js
@@ -0,0 +1,257 @@
+import { images } from "../constants";
+
+export const bookOtherWordsForHome = {
+  id: 1,
+  bookName: "Other Words For Home",
+  bookCover: images.otherWordsForHome,
+  rating: 4.5,
+  language: "Eng",
+  pageNo: 341,
+  author: "Jasmine Warga",
+  genre: ["Romance", "Adventure", "Drama"],
+  readed: "12k",
+  description:
+    "Jude never thought she’d be leaving her beloved older brother and father behind, all the way across the ocean in Syria. But when things in her hometown start becoming volatile, Jude and her mother are sent to live in Cincinnati with relatives. At first, everything in America seems too fast and too loud. The American movies that Jude has always loved haven’t quite prepared her for starting school in the US—and her new label of 'Middle Eastern,' an identity she’s never known before. But this life also brings unexpected surprises—there are new friends, a whole new family, and a school musical that Jude might just try out for. Maybe America, too, is a place where Jude can be seen as she really is.",
+  backgroundColor: "rgba(240,240,232,0.9)",
+  navTintColor: "#000",
+};
+
+export const bookTheMetropolis = {
+  id: 2,
+  bookName: "The Metropolis",
+  bookCover: images.theMetropolist,
+  rating: 4.1,
+  language: "Eng",
+  pageNo: 272,
+  author: "Seith Fried",
+  genre: ["Adventure", "Drama"],
+  readed: "13k",
+  description:
+    "In Metropolis, the gleaming city of tomorrow, the dream of the great American city has been achieved. But all that is about to change, unless a neurotic, rule-following bureaucrat and an irreverent, freewheeling artificial intelligence can save the city from a mysterious terrorist plot that threatens its very existence. Henry Thompson has dedicated his life to improving America's infrastructure as a proud employee of the United States Municipal Survey. So when the agency comes under attack, he dutifully accepts his unexpected mission to visit Metropolis looking for answers. But his plans to investigate quietly, quickly, and carefully are interrupted by his new partner: a day-drinking know-it-all named OWEN, who also turns out to be the projected embodiment of the agency's supercomputer. Soon, Henry and OWEN are fighting to save not only their own lives and those of the city's millions of inhabitants, but also the soul of Metropolis. The Municipalists is a thrilling, funny, and touching adventure story, a tour-de-force of imagination that trenchantly explores our relationships to the cities around us and the technologies guiding us into the future.",
+  backgroundColor: "rgba(247,239,219,0.9)",
+  navTintColor: "#000",
+};
+
+export const bookTheTinyDragon = {
+  id: 3,
+  bookName: "The Tiny Dragon",
+  bookCover: images.theTinyDragon,
+  rating: 3.5,
+  language: "Eng",
+  pageNo: 110,
+  author: "Ana C Bouvier",
+  genre: ["Drama", "Adventure", "Romance"],
+  readed: "13k",
+  description:
+    "This sketchbook for kids is the perfect tool to improve your drawing skills! Designed to encourage kids around the world to express their uniqueness through drawing, sketching or doodling, this sketch book is filled with 110 high quality blank pages for creations. Add some fun markers, crayons, and art supplies and you have the perfect, easy gift for kids!",
+  backgroundColor: "rgba(119,77,143,0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookAtomicHabits = {
+  id: 4,
+  bookName: "Atomic Habits",
+  bookCover: images.atomicHabits,
+  rating: 4.9,
+  language: "Eng",
+  pageNo: 288,
+  author: "James Clear",
+  genre: ["Self Help", "Motivation", "Productivity"],
+  readed: "275k",
+  description:
+    "World-renowned habits expert James Clear has discovered a simpler system for transforming your life. He knows that lasting change comes from the compound effect of hundreds of small decisions – doing two push-ups a day, waking up five minutes early, or holding a single short phone call. He calls them atomic habits. \n In this ground-breaking book, Clear reveals how these tiny changes will help you get 1 percent better every day. He uncovers a handful of simple life hacks (the forgotten art of Habit Stacking, the unexpected power of the Two Minute Rule, or the trick to entering the Goldilocks Zone) and delves into cutting-edge psychology and neuroscience to explain why they matter. Along the way, he tells inspiring stories of Olympic gold medalists, leading CEOs and distinguished scientists who have used the science of small habits to stay productive, motivated and happy.",
+  backgroundColor: "rgba(255, 255, 255, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookCantHurtMe = {
+  id: 5,
+  bookName: "Can't Hurt Me",
+  bookCover: images.cantHurtMe,
+  rating: 4.8,
+  language: "Eng",
+  pageNo: 366,
+  author: "David Goggins",
+  genre: ["Self Help", "Motivation", "Productivity"],
+  readed: "215k",
+  description:
+    "David Goggins is a retired Navy SEAL and the only member of the U.S. Armed Forces ever to complete SEAL training, U.S. Army Ranger School, and Air Force Tactical Air Controller training. Goggins has competed in more than sixty ultra-marathons, triathlons, and ultra-triathlons, setting new course records and regularly placing in the top five. A former Guinness World Record holder for completing 4,030 pull-ups in seventeen hours, he's a much-sought-after public speaker who's shared his story with the staffs of Fortune 500 companies, professional sports teams, and hundreds of thousands of students across the country. --This text refers to an out of print or unavailable edition of this title.",
+  backgroundColor: "rgba(0, 0, 0, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookEverythingIsFked = {
+  id: 6,
+  bookName: "Everything is F*cked Up",
+  bookCover: images.everythingIs,
+  rating: 4.2,
+  language: "Eng",
+  pageNo: 224,
+  author: "Mark Manson",
+  genre: ["Self Help", "Motivation", "Productivity"],
+  readed: "87k",
+  description:
+    "In everything is f*cked, manson turns his gaze from the inevitable flaws within each individual self to the endless calamities taking place in the world around us. Drawing from the pool of psychological research on these topics, as well as the timeless wisdom of philosophers such as Plato, Nietzsche, and Tom waits, he dissects religion and politics and the uncomfortable ways they have come to resemble one another. He looks at our relationships with money, entertainment and the Internet, and how too much of a good thing can psychologically eat us alive. He openly defies our definitions of faith, happiness, freedom even of hope itself. With his usual mix of erudition and where-the-f*ck-did-that-come-from humor, manson takes us by the collar and challenges us to be more honest with ourselves and connected with the world in ways we probably haven’t considered before. It’s another counterintuitive romp through the pain in our hearts and the stress of our soul. One of the great modern writers has produced another book that will set the agenda for years to come.",
+  backgroundColor: "rgba(36, 203, 211, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookFaultInOurStars = {
+  id: 7,
+  bookName: "Fault In Our Stars",
+  bookCover: images.faultInOurStars,
+  rating: 4.7,
+  language: "Eng",
+  pageNo: 352,
+  author: "John Green",
+  genre: ["Drama", "Romance"],
+  readed: "815k",
+  description:
+    "Despite the tumour-shrinking medical miracle that has bought her a few years, Hazel has never been anything but terminal, her final chapter inscribed upon diagnosis.But when a gorgeous plot twist named Augustus Waters suddenly appears at Cancer Kid Support Group, Hazel's story is about to be completely rewritten.Insightful, bold, irreverent, and raw, The Fault in Our Stars brilliantly explores the funny, thrilling, and tragic business of being alive and in love.",
+  backgroundColor: "rgba(38, 161, 218, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookFortyRulesOfLove = {
+  id: 8,
+  bookName: "The Forty Rules of Love",
+  bookCover: images.fortyRulesOfLove,
+  rating: 4.8,
+  language: "Eng",
+  pageNo: 368,
+  author: "Elif Shafak",
+  genre: ["Drama", "Romance", "History", "Fiction"],
+  readed: "95k",
+  description:
+    "Every true love and friendship is a story of unexpected transformation. If we are the same person before and after we loved, that means we haven't loved enough...\nElla Rubinstein has a husband, three teenage children, and a pleasant home. Everything that should make her confident and fulfilled. Yet there is an emptiness at the heart of Ella's life - an emptiness once filled by love.\nSo when Ella reads a manuscript about the thirteenth-century Sufi poet Rumi and Shams of Tabriz, and his forty rules of life and love, her world is turned upside down. She embarks on a journey to meet the mysterious author of this work.It is a quest infused with Sufi mysticism and verse, taking Ella and us into an exotic world where faith and love are heartbreakingly explored. . .",
+  backgroundColor: "rgba(189, 27, 42, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookImmortalsOfMeluha = {
+  id: 9,
+  bookName: "The Immortals of Meluha",
+  bookCover: images.meluha,
+  rating: 4.4,
+  language: "Eng",
+  pageNo: 415,
+  author: "Amish",
+  genre: ["Drama", "Myths", "History", "Fiction"],
+  readed: "323k",
+  description:
+    "1900 BC. In what modern Indians mistakenly call the Indus Valley Civilisation.The inhabitants of that period called it the land of Meluha – a near perfect empire created many centuries earlier by Lord Ram, one of the greatest monarchs that ever lived. This once proud empire and its Suryavanshi rulers face severe perils as its primary river, the revered Saraswati, is slowly drying to extinction. They also face devastating terrorist attacks from the east, the land of the Chandravanshis. To make matters worse, the Chandravanshis appear to have allied with the Nagas, an ostracised and sinister race of deformed humans with astonishing martial skills. The only hope for the Suryavanshis is an ancient legend: ‘When evil reaches epic proportions, when all seems lost, when it appears that your enemies have triumphed, a hero will emerge.’ Is the rough-hewn Tibetan immigrant Shiva, really that hero? And does he want to be that hero at all? Drawn suddenly to his destiny, by duty as well as by love, will Shiva lead the Suryavanshi vengeance and destroy evil? This is the first book in a trilogy on Shiva, the simple man whose karma re-cast him as our Mahadev, the God of Gods.",
+  backgroundColor: "rgba(68, 80, 76, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookSita = {
+  id: 10,
+  bookName: "Sita: The Warrior of Mithila",
+  bookCover: images.sita,
+  rating: 4.8,
+  language: "Eng",
+  pageNo: 366,
+  author: "Amish",
+  genre: ["Drama", "Myths", "History", "Fiction"],
+  readed: "357k",
+  description:
+    "She is the warrior we need. The Goddess we await. \nShe will defend Dharma. She will protect us. \nIndia, 3400 BCE. \nIndia is beset with divisions, resentment and poverty. The people hate their rulers. They despise their corrupt and selfish elite. Chaos is just one spark away. Outsiders exploit these divisions. Raavan, the demon king of Lanka, grows increasingly powerful, sinking his fangs deeper into the hapless Sapt Sindhu. \nTwo powerful tribes, the protectors of the divine land of India, decide that enough is enough. A saviour is needed. They begin their search. \nAn abandoned baby is found in a field. Protected by a vulture from a pack of murderous wolves. She is adopted by the ruler of Mithila, a powerless kingdom, ignored by all. Nobody believes this child will amount to much. But they are wrong. \nFor she is no ordinary girl. She is Sita. \nContinue the epic journey with Amish’s latest: A thrilling adventure that chronicles the rise of an adopted child, who became the prime minister. And then, a Goddess. \nThis is the second book in the Ram Chandra Series. A sequel that takes you back. Back before the beginning.",
+  backgroundColor: "rgba(51, 12, 56, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookSubtleArtOfNotGivingAFked = {
+  id: 11,
+  bookName: "The Subtle Art of not giving a F*ck",
+  bookCover: images.subtleArt,
+  rating: 4.5,
+  language: "Eng",
+  pageNo: 224,
+  author: "Mark Manson",
+  genre: ["Self Help", "Motivation", "Productivity"],
+  readed: "568k",
+  description:
+    "In this generation-defining self-help guide, a superstar blogger cuts through the crap to show us how to stop trying to be “positive” all the time so that we can truly become better, happier people. \nFor decades, we’ve been told that positive thinking is the key to a happy, rich life. “Fuck positivity,” Mark Manson says. “Let’s be honest, shit is fucked and we have to live with it.” In his wildly popular Internet blog, Manson doesn’t sugar-coat or equivocate. He tells it like it is—a dose of raw, refreshing, honest truth that is sorely lacking today. The Subtle Art of Not Giving a F**k is his antidote to the coddling, let’s-all-feel-good mind-set that has infected American society and spoiled a generation, rewarding them with gold medals just for showing up. \nManson makes the argument, backed both by academic research and well-timed poop jokes, that improving our lives hinges not on our ability to turn lemons into lemonade, but on learning to stomach lemons better. Human beings are flawed and limited—“not everybody can be extraordinary, there are winners and losers in society and some of it is not fair or your fault.” Manson advises us to get to know our limitations and accept them. Once we embraceour fears, faults and uncertainties, once we stop running and avoiding and start confronting painful truths, we can begin to find the courage, perseverance, honesty, responsibility, curiosity and forgiveness we seek. \nThere are only so many things we can give a fuck about so we need to figure out which ones really matter, Manson makes clear. While money is nice, caring about what you do with your life is better, because true wealth is about experience.",
+  backgroundColor: "rgba(248, 80, 2, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookThePalaceOfIllusions = {
+  id: 12,
+  bookName: "The Palace of Illusions",
+  bookCover: images.palaceOfIllusions,
+  rating: 4.2,
+  language: "Eng",
+  pageNo: 384,
+  author: "Chitra Banerjee Divakaruni",
+  genre: ["Drama", "Myths", "History", "Fiction"],
+  readed: "75k",
+  description:
+    "Be taken back to a time that is half history, half myth, and wholly magical … \nFirst published in 2008, Chitra Banerjee Divakaruni's The Palace of Illusions has received wide acclaim for giving a woman's take on the timeless tale that is the Mahabharata. Narrated by Panchaali, wife of the five Pandava brothers, the novel traces her life from fiery birth and lonely childhood, where her beloved brother is her only true companion; through her complicated friendship with the enigmatic Krishna; to marriage, motherhood and her secret attraction to the mysterious man who is her husbands’ most dangerous enemy. This tenth anniversary edition, complete with a new author’s note, celebrates once again a remarkable and deeply human story about a woman born into a man’s world.",
+  backgroundColor: "rgba(64, 122, 75, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookWishICouldTellYou = {
+  id: 13,
+  bookName: "Wish I Could Tell You",
+  bookCover: images.wishICouldTellYou,
+  rating: 4.1,
+  language: "Eng",
+  pageNo: 288,
+  author: "Durjoy Data",
+  genre: ["Drama", "Romance"],
+  readed: "45k",
+  description:
+    "A disillusioned and heartbroken Anusha finds herself in the small world of WeDonate.com. Struggling to cope with her feelings and the job of raising money for charity, she reluctantly searches for a worthwhile cause to support. \nFor Ananth, who has been on the opposite side, no life is less worthy, no cause too small to support. \nBehind them are teams for whom going to extraordinary lengths to save lives is more than a full-time occupation. In front of them is the virtual world of social media-watching, interacting, judging, making choices, and sometimes, saving lives. \nFrom the virtual to the real, their lives and that of their families, entangle in a way that moving together is the only solution. They can't escape each other. \nIn this world of complicated relationships, should love be such a difficult ride?",
+  backgroundColor: "rgba(187, 129, 36, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookChamberOfSecrets = {
+  id: 14,
+  bookName: "Harry Potter and the Chamber Of Secrets",
+  bookCover: images.chamberOfSecrets,
+  rating: 4.9,
+  language: "Eng",
+  pageNo: 384,
+  author: "J.K. Rowling",
+  genre: ["Drama", "Action", "Adventure"],
+  readed: "45k",
+  description:
+    "Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft and Wizardry for his second year, Harry hears strange whispers echo through empty corridors - and then the attacks start. Students are found as though turned to stone . Dobby's sinister predictions seem to be coming true. \nThese new editions of the classic and internationally bestselling, multi-award-winning series feature instantly pick-up-able new jackets by Jonny Duddle, with huge child appeal, to bring Harry Potter to the next generation of readers. It's time to PASS THE MAGIC ON .",
+  backgroundColor: "rgba(126, 79, 88, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookDaVinciCode = {
+  id: 15,
+  bookName: "The Da Vinci Code",
+  bookCover: images.daVinciCode,
+  rating: 4.3,
+  language: "Eng",
+  pageNo: 592,
+  author: "Dan Brown",
+  genre: ["Drama", "Mystery", "Horror"],
+  readed: "752k",
+  description:
+    "The record-breaking bestseller. \nA man is murdered in the world’s most famous museum. \nAround his body is a ring of codes, drawn in blood. \nHe died to protect a secret which Robert Langdon must uncover. \nIt will be a race against time to decipher this final message. \nCan he get there before the killers do?",
+  backgroundColor: "rgba(254, 250, 189, 0.9)",
+  navTintColor: "#FFF",
+};
+
+export const bookTheHobbit = {
+  id: 16,
+  bookName: "The Hobbit",
+  bookCover: images.theHobbit,
+  rating: 5.0,
+  language: "Eng",
+  pageNo: 400,
+  author: "J.R.R.Tolkien",
+  genre: ["Adventure", "Drama", "Action"],
+  readed: "975k",
+  description:
+    "Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely travelling further than the pantry of his hobbit-hole in Bag End. But his contentment is disturbed when the wizard, Gandalf and a company of thirteen dwarves arrive on his doorstep one day to whisk him away on an unexpected journey ‘there and back again'. They have a plot to raid the treasure hoard of Smaug the Magnificent, a large and very dangerous dragon. The prelude to The Lord of the Rings, The Hobbit has sold many millions of copies since its publication in 1937, establishing itself as one of the most beloved and influential books of the twentieth century.",
+  backgroundColor: "rgba(220, 179, 89, 0.9)",
+  navTintColor: "#FFF",
+};
